a9ecb1a937e87e682199780796a0ea52e6756116
Miguel Ramos <return.infox@gmail.com>
2025-06-06 09:45:13 -0400

    solucion

--- ejercicio-4/solucion.sql
+++ ejercicio-4/solucion.sql
@@ -8,3 +8,8 @@ GO
 INSERT INTO Inscripciones (IDEstudiante, IDCurso, FechaInscripcion) -- Inserta un registro nuevo
 VALUES (1, 2, '2024-06-03'); -- Datos del registro insertado
 GO
+
+BACKUP LOG AcademiaEjemploDB -- Hace un backup solo del log de transacciones 
+TO DISK = 'C:\Backups\AcademiaEjemploDB_Estrategia3.bak' -- Usa el mismo archivo, se agrega como otro "segmento"
+WITH NAME = 'LogBackup1'; -- Nombre para identificar este backup de log
+GO
