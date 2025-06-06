074c49e7a794b88c6ab0e3702d43e956d83a4c26
Miguel Ramos <return.infox@gmail.com>
2025-06-06 09:42:46 -0400

    solucion

--- ejercicio-4/solucion.sql
+++ ejercicio-4/solucion.sql
@@ -3,3 +3,8 @@ TO DISK = 'C:\Backups\AcademiaEjemploDB_Estrategia3.bak' -- Ruta donde se guarda
 WITH INIT, -- Sobrescribe el archivo de backup si ya existe
 	 NAME = 'FullBackup'; -- Nombre descriptivo para identificar este backup
 GO
+
+-- Cambios posteriores
+INSERT INTO Inscripciones (IDEstudiante, IDCurso, FechaInscripcion) -- Inserta un registro nuevo
+VALUES (1, 2, '2024-06-03'); -- Datos del registro insertado
+GO
