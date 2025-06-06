a7f2dbb9cc60746c22b2aafb11d9450fb337874f
Miguel Ramos <return.infox@gmail.com>
2025-06-06 10:12:07 -0400

    cambios posteriores

---
+++ ejercicio-4/biblioteca/cambios-posteriores.sql
@@ -0,0 +1,16 @@
+-- 📝 Insertamos datos para simular cambios (1)
+INSERT INTO operaciones.Prestamos (IDLibro, IDUsuario, FechaPrestamo)
+VALUES (5, 4, '2024-06-06'), (6, 5, '2024-06-06');
+GO
+
+
+-- 📝 Nuevos cambios (2)
+INSERT INTO operaciones.Prestamos (IDLibro, IDUsuario, FechaPrestamo)
+VALUES (7, 6, '2024-06-07'), (8, 7, '2024-06-07');
+GO
+
+
+-- 📝 Más actividad en la base (3)
+INSERT INTO operaciones.Prestamos (IDLibro, IDUsuario, FechaPrestamo)
+VALUES (9, 8, '2024-06-08'), (10, 9, '2024-06-08');
+GO
